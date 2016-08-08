ef686e868362b1c522ed5bb6f89e1080496cceb8
Nikola Kantar <nkantar@gmail.com>
2016-08-07 21:43:28 -0700

    Add moar aliases

--- aliases.sh
+++ aliases.sh
@@ -9,6 +9,8 @@ alias cll="cls && ls -l"
 # applications
 alias v='vim'
 alias g='git'
+alias p='python'
+alias pi='pip'
 
 # git
 alias gitprune='git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d'
