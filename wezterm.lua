f57ce2f6e71b5a6e164e44ac9239a7179f41217b
Nik Kantar <nik@instil.io>
2022-01-13 09:49:15 -0800

    Update WezTerm key mappings for splits and tabs

--- wezterm.lua
+++ wezterm.lua
@@ -23,14 +23,22 @@ return {
     -- option -> meta
     { key="Alt", mods="META", action="DisableDefaultAssignment" },
 
-    -- horizontal split
-    {key="%", mods="ALT|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-
-    -- vertical split
-    {key="\"", mods="ALT|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-
     -- zoom pane
     { key = "Z", mods="ALT|SHIFT", action="TogglePaneZoomState" },
+
+    -- splits
+    { key="|", mods="CTRL|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
+    { key="_", mods="CTRL|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
+
+    -- switch pane
+    { key = "H", mods="CTRL|SHIFT", action=wezterm.action{ActivatePaneDirection="Left"}},
+    { key = "J", mods="CTRL|SHIFT", action=wezterm.action{ActivatePaneDirection="Down"}},
+    { key = "K", mods="CTRL|SHIFT", action=wezterm.action{ActivatePaneDirection="Up"}},
+    { key = "L", mods="CTRL|SHIFT", action=wezterm.action{ActivatePaneDirection="Right"}},
+
+    -- switch tab
+    { key="{", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1} },
+    { key="}", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1} },
   },
   send_composed_key_when_left_alt_is_pressed=true,
   send_composed_key_when_right_alt_is_pressed=true,
