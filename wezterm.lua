a36ec7d87e0bbe50cb9667a624a9601a1c294bfc
Nik Kantar <nik@nkantar.com>
2022-02-02 19:17:32 -0800

    Revert "Update WezTerm key mappings"
    This reverts commit f4b9e3e20d95e09f3dc44eead3855b12220d2ef3.

--- wezterm.lua
+++ wezterm.lua
@@ -24,21 +24,21 @@ return {
     { key="Alt", mods="META", action="DisableDefaultAssignment" },
 
     -- zoom pane
-    { key = "M", mods="ALT|SHIFT", action="TogglePaneZoomState" },
+    { key = "Z", mods="ALT|SHIFT", action="TogglePaneZoomState" },
 
     -- splits
-    { key="|", mods="ALT|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
-    { key="_", mods="ALT|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
+    { key="|", mods="CTRL|SHIFT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
+    { key="_", mods="CTRL|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
 
     -- switch pane
-    { key = "H", mods="ALT|SHIFT", action=wezterm.action{ActivatePaneDirection="Left"}},
-    { key = "J", mods="ALT|SHIFT", action=wezterm.action{ActivatePaneDirection="Down"}},
-    { key = "K", mods="ALT|SHIFT", action=wezterm.action{ActivatePaneDirection="Up"}},
-    { key = "L", mods="ALT|SHIFT", action=wezterm.action{ActivatePaneDirection="Right"}},
+    { key = "H", mods="CTRL|SHIFT", action=wezterm.action{ActivatePaneDirection="Left"}},
+    { key = "J", mods="CTRL|SHIFT", action=wezterm.action{ActivatePaneDirection="Down"}},
+    { key = "K", mods="CTRL|SHIFT", action=wezterm.action{ActivatePaneDirection="Up"}},
+    { key = "L", mods="CTRL|SHIFT", action=wezterm.action{ActivatePaneDirection="Right"}},
 
     -- switch tab
-    { key="{", mods="ALT|SHIFT", action=wezterm.action{ActivateTabRelative=-1} },
-    { key="}", mods="ALT|SHIFT", action=wezterm.action{ActivateTabRelative=1} },
+    { key="{", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1} },
+    { key="}", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1} },
   },
   send_composed_key_when_left_alt_is_pressed=true,
   send_composed_key_when_right_alt_is_pressed=true,
