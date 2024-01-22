5555fc721cf09554648158d32822c7f23d60b537
Nik Kantar <nik@reach.security>
2024-01-22 11:35:29 -0800

    Fix fish config startup issues

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,10 @@
 # source ~/.env_vars.sh
 
+set PATH $PATH /opt/homebrew/bin
+set PATH $PATH ~/bin
+
+set -Ux EDITOR (which hx)
+
 set -U fish_greeting
 
 if status is-interactive
@@ -10,8 +15,6 @@ starship init fish | source
 
 # Created by `pipx` on 2021-12-15 08:09:28
 set PATH $PATH /Users/nik/.local/bin
-
-set EDITOR (which hx)
 
 set DOTFILES ~/.dotfiles
 
